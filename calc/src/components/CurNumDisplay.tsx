e4bf459149a7572c7c880b75576ecd5f211e899b
MEBonham <programmer@mckaybonham.com>
2022-05-30 13:53:25 -0600

    Only displays empty number as 0 when cleared.

--- calc/src/components/CurNumDisplay.tsx
+++ calc/src/components/CurNumDisplay.tsx
@@ -5,12 +5,13 @@ import useInputs from '../store/InputsStore';
 
 const CurNumDisplay: React.FC = () => {
     const num: string = useInputs((state) => state.numSoFar);
+    const history: string[] = useInputs((state) => state.history);
     const finished = useInputs((state) => state.finished);
     const ans = useInputs((state) => state.ans);
     const [numToDisplay, setNumToDisplay] = React.useState("0");
 
     React.useEffect(() => {
-        setNumToDisplay(num === "" ? "0" : num);
+        setNumToDisplay(num === "" && history.length === 0 ? "0" : num);
     }, [num])
 
     return(
